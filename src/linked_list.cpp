9d77f3f96de55493f18b2153aa41b9a2d37842ac
Lena Gavrilova <lena.gavrilova2408@mail.ru>
2022-04-20 17:37:20 +0300

    done

--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -10,10 +10,25 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseIterative() {
-    // Напишите здесь свой код ...
+    if (size_ > 0) {
+      Node* curr_node = front_;
+      Node* prev_node = nullptr;
+
+      while (curr_node != back_) {
+        Node* next = curr_node->next;
+        curr_node->next = prev_node;
+        prev_node = curr_node;
+        curr_node = next;
+      }
+      curr_node->next = prev_node;
+
+      Node* back_node = back_;
+      back_ = front_;
+      front_ = back_node;
+    }
   }
 
-  /**
+  /*
    * Переворачивает связный список, используя рекурсию.
    *
    * Сложность
@@ -21,12 +36,22 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseRecursive() {
-    // Напишите здесь свой код ...
+    if (size_ > 0) {
+      reverse_recursive_helper(front_, nullptr);
+      Node* back_node = back_;
+      back_ = front_;
+      front_ = back_node;
+    }
   }
 
   // вспомогательный метод для реализации рекурсии
   void LinkedList::reverse_recursive_helper(Node*& curr, Node* prev) {
-    // Напишите здесь свой код ...
+    Node* node = curr->next;
+    curr->next = prev;
+
+    if (node != nullptr) {
+      reverse_recursive_helper(node, curr);
+    }
   }
 
 }  // namespace assignment
